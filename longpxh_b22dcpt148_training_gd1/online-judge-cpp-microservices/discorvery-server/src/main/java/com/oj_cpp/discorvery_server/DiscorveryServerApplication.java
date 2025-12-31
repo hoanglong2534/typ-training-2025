21589e412d12480dd936d72d7ace981db62fdd2d
hoanglong2534 <phamxuanhoanglong@gmail.com>
2025-12-31 17:29:07 +0700

    feat(sys): add eureka discovery server

---
+++ longpxh_b22dcpt148_training_gd1/online-judge-cpp-microservices/discorvery-server/src/main/java/com/oj_cpp/discorvery_server/DiscorveryServerApplication.java
@@ -0,0 +1,15 @@
+package com.oj_cpp.discorvery_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscorveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscorveryServerApplication.class, args);
+	}
+
+}
---
+++ longpxh_b22dcpt148_training_gd1/online-judge-cpp-microservices/discorvery-server/src/test/java/com/oj_cpp/discorvery_server/DiscorveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oj_cpp.discorvery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscorveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
