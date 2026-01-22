b0eee45280550c9d36cda0ac2bb81086e99b50f5
hoanglong2534 <hoanglong2534@users.noreply.github.com>
2026-01-22 16:41:56 +0700

    feat(judge): add judge service

---
+++ longpxh_b22dcpt148_training_gd1/online-judge-cpp-microservices/judge-service/judge-domain/src/main/java/com/oj_cpp/judge/domain/enums/JudgeResultEnum.java
@@ -0,0 +1,5 @@
+package com.oj_cpp.judge.domain.enums;
+
+public enum JudgeResultEnum {
+    AC, WA, TLE, MLE, RE, CE
+}
---
+++ longpxh_b22dcpt148_training_gd1/online-judge-cpp-microservices/judge-service/judge-domain/src/main/java/com/oj_cpp/judge/domain/enums/LanguageJudgeEnum.java
@@ -0,0 +1,5 @@
+package com.oj_cpp.judge.domain.enums;
+
+public enum LanguageJudgeEnum {
+    CPP
+}
